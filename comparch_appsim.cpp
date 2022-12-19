b2a9517b6a4ee7455f02adcfec498b18230690c6
Kevin Hack <98929190+quackb83@users.noreply.github.com>
2022-12-19 12:56:00 -0500

    Update comparch_appsim.cpp

--- comparch_appsim.cpp
+++ comparch_appsim.cpp
@@ -25,20 +25,21 @@ int main() {
 
 	Computer a;
 	a.cacheHitR = 0.95;
-	a.cacheMissP = 10;
+	a.cacheMissP = 6;
 
 	Computer b;
-	b.cacheHitR = 0.92;
-	b.cacheMissP = 8;
+	b.cacheHitR = 0.96;
+	b.cacheMissP = 7;
 
 	//testing computers with program 1
-	cout << "Computer A, Program 1: " << cycles(2000, 0.2, a) << endl;
-	cout << "Computer B, Program 1: " << cycles(2000, 0.2, b) << endl;
+	cout << "Computer A, Program 1: " << cycles(1500, 0.25, a) << endl;
+	cout << "Computer B, Program 1: " << cycles(1500, 0.25, b) << endl;
 
 	//testing computers with program 2
-	cout << "Computer A, Program 2: " << cycles(2000, 0.4, a) << endl;
-	cout << "Computer B, Program 2: " << cycles(2000, 0.4, b) << endl;
+	cout << "Computer A, Program 2: " << cycles(1500, 0.3, a) << endl;
+	cout << "Computer B, Program 2: " << cycles(1500, 0.3, b) << endl;
 
+	/*
 	//testing computers with program 3
 	cout << "Computer A, Program 3: " << cycles(2000, 0.6, a) << endl;
 	cout << "Computer B, Program 3: " << cycles(2000, 0.6, b) << endl;
@@ -46,16 +47,19 @@ int main() {
 	//testing computers with program 4
 	cout << "Computer A, Program 4: " << cycles(2000, 0.8, a) << endl;
 	cout << "Computer B, Program 4: " << cycles(2000, 0.8, b) << endl;
-
+	*/
 }
 
 int cycles(int&& instr, double&& mem, const Computer& c) {
 
-	const int memOps = instr * mem;
-	const int non_memOps = instr - memOps;
-	const int cacheHits = memOps * c.cacheHitR;
-	const int cacheMiss = memOps - cacheHits;
-	const int cycles = (non_memOps)+(memOps * 2)+(cacheMiss * c.cacheMissP);
+	//used std::ceil to round up (as instructed on exam)
+	int memOps = ceil(instr * mem);
+	int non_memOps = instr - memOps;
+	//used std::ceil to round up (as instructed on exam)
+	int cacheHits = ceil(memOps * c.cacheHitR);
+	int cacheMiss = memOps - cacheHits;
+	//used std::ceil to round up (as instructed on exam)
+	int cycles = ceil((non_memOps)+(memOps * 2)+(cacheMiss * c.cacheMissP));
 
 	return cycles;
 }
